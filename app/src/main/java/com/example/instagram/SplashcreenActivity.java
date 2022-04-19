2a7cc4d7e16419079673650a9cf6214b8370b4f8
sunsetjkyle <sunsetkyle@protonmail.com>
2022-04-19 15:04:36 +0300

    Splashscreeen Activity

---
+++ app/src/main/java/com/example/instagram/SplashcreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashcreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashcreen);
+
+        new Handler().postDelayed(() -> startActivity(new Intent(SplashcreenActivity.this, LandngActivity.class)), 1000);
+    }
+}
