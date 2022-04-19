9c0fdc7aa21d7508bee7c7181d629f0d832a6c1a
sunsetjkyle <sunsetkyle@protonmail.com>
2022-04-19 15:55:45 +0300

    Landing activity and xml

--- app/src/main/java/com/example/instagram/LandngActivity.java
+++ app/src/main/java/com/example/instagram/LandngActivity.java
@@ -1,14 +1,33 @@
 package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LandngActivity extends AppCompatActivity {
+    private AppCompatButton create;
+    private TextView txt_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landng);
+        create = findViewById(R.id.btn_create);
+        txt_login = findViewById(R.id.txt_login);
+
+        create.setOnClickListener(view -> {
+            startActivity(new Intent(LandngActivity.this, RegisterActivity.class));
+            finish();
+
+        });
+
+        txt_login.setOnClickListener(view -> {
+            startActivity(new Intent(LandngActivity.this, LoginActivity.class));
+            finish();
+        });
     }
 }
